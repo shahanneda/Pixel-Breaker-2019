efd8dfdb4a2f9bcf4b5fea6a9bd3ead32cd5b478
Shahan Neda <shahan.neda@gmail.com>
2019-04-16 16:07:50 -0700

    added tile generation

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    List<Tile> tiles = new List<Tile>();
+    public int gameWidth = 10;
+    public int gameHeight = 20;
+
+    public float tileSpaceX = 1;//TODO: maybe make this  auto generated for optimal setting maybe?
+    public float tileSpaceY = 1;
+
+    public GameObject tilePrefab;
+
+    Tile[,] tiles;
     // Start is called before the first frame update
     void Start()
     {
-		foreach(Transform t in transform){
-            tiles.Add(t.GetComponent<Tile>());
-		}
+        tiles = new Tile [gameWidth,gameHeight];
+        //TODO:: Level editor maybe? maybe also refactor this function to diffrent file
+        FillTiles();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    private void FillTiles(){
+        for (int i = 0; i < tiles.GetLength(0); i++){
+            for (int k = 0; k < tiles.GetLength(1); k++){
+                tiles[i,k] = (Instantiate(tilePrefab,
+                      new Vector2(transform.position.x + i * tileSpaceX, transform.position.y + k * tileSpaceY), 
+                      Quaternion.identity, transform) as GameObject)
+                    .GetComponent<Tile>();                              
+            }
+        }
 
     }
 }
