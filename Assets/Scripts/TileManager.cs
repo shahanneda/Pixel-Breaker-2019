d877c65ec404fb713caed5cd0eb5d52e6f93f1e0
Shahan Neda <shahan.neda@gmail.com>
2019-04-16 15:45:40 -0700

    Added simple tile and tileManager.

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+	//List<Tile> tiles = new ArrayList<Tile>();
+    // Start is called before the first frame update
+    void Start()
+    {
+		foreach(Transform t in transform){
+			//tiles.add(t.<Tile>());
+
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
