4e60737ea27d575dc0b5bbe8c9c1c3f37076641c
Adam Pod <adam.podoxin@gmail.com>
2019-04-19 16:31:16 -0700

    Cleaned up code

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,91 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//THIS CLASS IS HERE JUST FOR THE BUTTON CLICKS, BUT TILES THEMSELVES SHOULD NOT HANDLE ANY ACTIONS, ISNTEAD
+//THEY SHOULD REPORT THAT MESSAGE TO THEIR MANAGER!
 public class Tile : MonoBehaviour
 {
     public static TileManager manager;
 
-    [HideInInspector]
-    public int X; 
-    [HideInInspector]
-    public int Y;
+    [HideInInspector] public int X;
+    [HideInInspector] public int Y;
 
     public float speed = 1f;
 
     public bool inAnimation = false;
     public bool isDead = false;
 
-    [HideInInspector]
-    public Vector2 shouldMoveTo = Vector2.positiveInfinity;
-    [HideInInspector]
-    public Color color;
+    [HideInInspector] public Vector2 shouldMoveTo = Vector2.positiveInfinity;
+    [HideInInspector] public Color color;
 
-    private Animator anim;
-    private SpriteRenderer renderer;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
-    private bool _isFalling;
-    public bool isFalling{
+    public bool IsFalling { get; set; }
 
-        get{
-            return _isFalling;
-        }
-        set{
-            _isFalling = value;
-            //if(_isFalling)
-            //{
-            //    speed = 1f;
-            //}else{
-            //    speed = 1f;
-            //}
-        }
-
-    }
-    //THIS CLASS IS HERE JUST FOR THE BUTTON CLICKS, BUT TILES THEMSELVES SHOULD NOT HANDLE ANY ACTIONS, ISNTEAD
-    //THEY SHOULD REPORT THAT MESSAGE TO THEIR MANAGER!
-    void OnMouseDown(){
+    void OnMouseDown()
+    {
         manager.HandleTileClick(this);
     }
+
     public void Start()
     {
-
-        if(!isDead){
-            GetComponent<SpriteRenderer>().color = manager.colors[Random.Range(0, manager.colors.Length - 1)];
-            renderer = GetComponent<SpriteRenderer>();
-            this.color = renderer.color;
-            anim = GetComponent<Animator>();
+        if (!isDead)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.color = manager.colors[Random.Range(0, manager.colors.Length - 1)];
+            color = spriteRenderer.color;
+            animator = GetComponent<Animator>();
         }
-
-         
-
     }
+
     public void Update()
     {
-        if(inAnimation){
+        if (inAnimation)
+        {
             transform.position = Vector2.Lerp(transform.position, shouldMoveTo, speed * Time.deltaTime);
         }
-        if(Vector2.Distance(transform.position, shouldMoveTo) < 0.01f){
+
+        if (Vector2.Distance(transform.position, shouldMoveTo) < 0.01f)
+        {
             inAnimation = false;
-            isFalling = false;
+            IsFalling = false;
         }
     }
-    public void setIsDead(){
+
+    public void SetIsDead()
+    {
         isDead = true;
-        GetComponent<SpriteRenderer>().enabled = false;
+        spriteRenderer.enabled = false;
     }
+
     public void OnMouseOver()
     {
         if (isDead)
             return;
-        anim.SetBool("isHover", true);
-        renderer.sortingOrder = 999;
+
+        animator.SetBool("isHover", true);
+        spriteRenderer.sortingOrder = 999;
 
     }
+
     public void OnMouseExit()
     {
         if (isDead)
             return;
 
-        anim.SetBool("isHover", false);
-        renderer.sortingOrder = 0;
+        animator.SetBool("isHover", false);
+        spriteRenderer.sortingOrder = 0;
     }
 }
