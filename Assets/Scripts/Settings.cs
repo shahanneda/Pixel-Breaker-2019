a5e9ea03a8e764bcc07c38b0fd26351b4aef7036
Adam Pod <adam.podoxin@gmail.com>
2019-08-08 15:03:53 -0700

    Made settings save to file

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
+    public SettingsFile settingsFile;
+    private bool settingsFileExists = false;
+
     public GameObject[] menus;
 
     public Resolution[] resolutions;
@@ -23,18 +27,29 @@ public class Settings : MonoBehaviour
     public Text sfxVolumeText;
     public Slider sfxVolumeSlider;
 
+    private string filePath;
+
     private void Start()
     {
+        filePath = Application.persistentDataPath + "/Settings.json";
+
         SetUp();
     }
 
     private void SetUp()
     {
+        LoadSettings();
+
         SetUpResolutions();
         SetUpFullscreen();
 
         SetUpMusic();
         SetUpSFX();
+
+        if (settingsFile != null)
+        {
+            ApplySettings();
+        }
     }
 
     private void SetUpResolutions()
@@ -43,36 +58,73 @@ public class Settings : MonoBehaviour
 
         List<string> resolutionOptions = new List<string>();
         int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
+
+        foreach (Resolution resolution in resolutions)
         {
-            resolutionOptions.Add(resolutions[i].width + " x " + resolutions[i].height);
-            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height) currentResolutionIndex = i;
+            resolutionOptions.Add(resolution.width + " x " + resolution.height);
         }
 
         resolutionsDropdown.AddOptions(resolutionOptions);
+
+        if (!settingsFileExists)
+        {
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height) currentResolutionIndex = i;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                if (resolutions[i].width == settingsFile.width && resolutions[i].height == settingsFile.height) currentResolutionIndex = i;
+            }
+        }
+
         resolutionsDropdown.value = currentResolutionIndex;
         resolutionsDropdown.RefreshShownValue();
     }
 
     private void SetUpFullscreen()
     {
-        fullscreenToggle.isOn = Screen.fullScreen;
+        if (!settingsFileExists)
+        {
+            fullscreenToggle.isOn = Screen.fullScreen;
+        }
+        else
+        {
+            fullscreenToggle.isOn = settingsFile.fullscreen;
+        }
     }
 
     private void SetUpMusic()
     {
-        musicMixer.GetFloat("Volume", out float volume);
+        if (!settingsFileExists)
+        {
+            musicMixer.GetFloat("Volume", out float volume);
 
-        musicVolumeSlider.value = (volume + 80) * 100;
-        ChangeMusicVolume(musicVolumeSlider.value);
+            musicVolumeSlider.value = (volume + 80) * 100;
+            ChangeMusicVolume(musicVolumeSlider.value);
+        }
+        else
+        {
+            musicVolumeSlider.value = settingsFile.musicVolume;
+        }
     }
 
     private void SetUpSFX()
     {
-        sfxMixer.GetFloat("Volume", out float volume);
+        if (!settingsFileExists)
+        {
+            sfxMixer.GetFloat("Volume", out float volume);
 
-        sfxVolumeSlider.value = (volume + 80) * 100;
-        ChangeSFXVolume(sfxVolumeSlider.value);
+            sfxVolumeSlider.value = (volume + 80) * 100;
+            ChangeSFXVolume(sfxVolumeSlider.value);
+        }
+        else
+        {
+            sfxVolumeSlider.value = settingsFile.sfxVolume;
+        }
     }
 
     private void ApplyResolutionAndFullscreen()
@@ -89,6 +141,26 @@ public class Settings : MonoBehaviour
     private void ApplySFXVolume()
     {
         sfxMixer.SetFloat("Volume", 0.8f * sfxVolumeSlider.value - 80);
+    }
+
+    public void SaveSettings()
+    {
+        settingsFile = new SettingsFile();
+        settingsFile.SetSettings(resolutions[resolutionsDropdown.value], fullscreenToggle.isOn, (int)musicVolumeSlider.value, (int)sfxVolumeSlider.value);
+
+        string json = JsonUtility.ToJson(settingsFile);
+        File.WriteAllText(filePath, json);
+    }
+
+    private void LoadSettings()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            settingsFile = JsonUtility.FromJson<SettingsFile>(json);
+
+            settingsFileExists = true;
+        }
     }
 
     public void ChangeMusicVolume(float volume)
@@ -116,5 +188,41 @@ public class Settings : MonoBehaviour
             if (m.Equals(menu)) m.SetActive(true);
             else m.SetActive(false);
         }
+    }
+}
+
+[System.Serializable]
+public class SettingsFile
+{
+    public int width;
+    public int height;
+    public bool fullscreen;
+
+    public int musicVolume;
+    public int sfxVolume;
+
+    public SettingsFile()
+    {
+
+    }
+
+    public SettingsFile(int width, int height, bool fullscreen, int musicVolume, int sfxVolume)
+    {
+        this.width = width;
+        this.height = height;
+        this.fullscreen = fullscreen;
+
+        this.musicVolume = musicVolume;
+        this.sfxVolume = sfxVolume;
+    }
+
+    public void SetSettings(Resolution resolution, bool fullscreen, int musicVolume, int sfxVolume)
+    {
+        width = resolution.width;
+        height = resolution.height;
+        this.fullscreen = fullscreen;
+
+        this.musicVolume = musicVolume;
+        this.sfxVolume = sfxVolume;
     }
 }
