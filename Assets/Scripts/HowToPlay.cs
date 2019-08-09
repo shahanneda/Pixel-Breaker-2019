d8151c337ba6a1d593b26672c4e54fdf483f2abd
Adam Pod <adam.podoxin@gmail.com>
2019-08-08 18:25:03 -0700

    Created tutorial system and added 2 tutorial videos

---
+++ Assets/Scripts/HowToPlay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class HowToPlay : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Text tutorialText;
+
+    public void PlayTutorialClip(VideoClip videoClip)
+    {
+        videoPlayer.gameObject.SetActive(true);
+        videoPlayer.clip = videoClip;
+        videoPlayer.Play();
+    }
+
+    public void ShowText(string text)
+    {
+        tutorialText.text = text;
+    }
+
+    public void StopTutorialClip()
+    {
+        videoPlayer.Stop();
+        videoPlayer.clip = null;
+        videoPlayer.gameObject.SetActive(false);
+    }
+
+    public void HideText()
+    {
+        tutorialText.text = "";
+    }
+}
