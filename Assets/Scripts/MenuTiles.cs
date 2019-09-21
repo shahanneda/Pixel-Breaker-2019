675528486ac9b071b6ac5cc101f4e867956e148a
Adam Pod <adam.podoxin@gmail.com>
2019-09-20 20:28:31 -0700

    Created menu tiles interaction

---
+++ Assets/Scripts/MenuTiles.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuTiles : MonoBehaviour
+{
+    public GameObject[] tiles;
+
+    private float maxX = 0;
+    private float maxY = 0;
+
+    private int previousIndex = -1;
+
+    private void Start()
+    {
+        StartCoroutine(ShowTile());
+
+        maxX = 0.468f * Screen.width;
+        maxY = 0.444f * Screen.height;
+    }
+
+    private IEnumerator ShowTile()
+    {
+        yield return new WaitForSeconds(Random.Range(3f, 8f));
+
+        int index = Random.Range(0, tiles.Length);
+
+        while (index == previousIndex)
+        {
+            index = Random.Range(0, tiles.Length);
+        }
+
+        previousIndex = index;
+
+        GameObject tile = tiles[index];
+        tile.GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(-maxX, maxX), Random.Range(-maxY, maxY));
+        tile.GetComponent<Animator>().Play("Fade In");
+
+        StartCoroutine(ShowTile());
+    }
+}
